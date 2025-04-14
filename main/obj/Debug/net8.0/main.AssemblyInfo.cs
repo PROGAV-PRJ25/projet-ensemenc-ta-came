c1cb298a7b8a38b1ab6305d84c45186fd61e7d2f
ta-came <cavignes@ensc.fr>
2025-04-14 16:25:50 +0200

    carcassone chateau

--- main/obj/Debug/net8.0/main.AssemblyInfo.cs
+++ main/obj/Debug/net8.0/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3010e5d621139b44e70f69f1342302c6f85370d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590b3ad8bcecc129d96e215b3e76e4191632a3c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
