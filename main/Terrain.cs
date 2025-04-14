28c1eba8330da2a3fb6962a131733f1d6341d457
Thiméo Tonon <ttonon@ensc.fr>
2025-04-15 01:25:56 +0200

    Création du terrain avec deux méthdes non réalisée qui permettent l'ajout ou le retrait de plantes dans le champs

---
+++ main/Terrain.cs
@@ -0,0 +1,11 @@
+public abstract class Terrain(){
+    public Plante[][]? parcelles;
+    public void Ajouter(Plante semis,int[] coordonnéees)
+    {
+        //TODO
+        //ajouter au parcelles la plante
+    }
+    public void Retirer(Plante plante){
+        //retirer une plante
+    }
+}
