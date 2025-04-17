fcef2ab19f685bbdc592b3accb27ed697721c125
Thiméo Tonon <ttonon@ensc.fr>
2025-04-17 10:28:52 +0200

    déplacement du dessin du chateau dans un fichier .txt

--- main/Accueil.cs
+++ main/Accueil.cs
@@ -4,47 +4,7 @@ public class Accueil
 
  public override string ToString()
      {
-// TitrePrincipal+=@"                                     ____                                         "+"\n";
-// TitrePrincipal+=@"                                 .-"     `-.      ,                               "+"\n";
-// TitrePrincipal+=@"                               .'           '.   /j\                              "+"\n";
-// TitrePrincipal+=@"                              /               \,/:/#\                /\           "+"\n";
-// TitrePrincipal+=@"                             ;               ,//' '/#\              //#\          "+"\n";
-// TitrePrincipal+=@"                            |               /' :   '/#\            /  /#\         "+"\n";
-// TitrePrincipal+=@"                             :          ,  /' /'    '/#\__..--""""/    /#\__      "+"\n";
-// TitrePrincipal+=@"                              \        /'\'-._:__    '/#\        ;      /#, """---"+"\n";
-// TitrePrincipal+=@"                               `-.    / ;#\']" ; """--./#J       ':____...!       "+"\n";
-// TitrePrincipal+=@"                                  `- /   /#\  J  [;[;[;Y]         |      ;        "+"\n";
-// TitrePrincipal+=@"    """"""---....             __.--"/    '/#\ ;   " "  |     !    |   #! |        "+"\n";
-// TitrePrincipal+=@"                 ""--.. _.--""     /      ,/#\'-..____.;_,   |    |   '  |        "+"\n";
-// TitrePrincipal+=@"                       "-.        :_....___,/#} "####" | '_.-",   | # '  |        "+"\n";
-// TitrePrincipal+=@"                          '-._      |[;[;[;[;|         |.;'  /;\  |      |        "+"\n";
-// TitrePrincipal+=@"                          ,   `-.   |        :     _   .;'    /;\ |   #" |        "+"\n";
-// TitrePrincipal+=@"                          !      `._:      _  ;   ##' .;'      /;\|  _,  |        "+"\n";
-// TitrePrincipal+=@"                         .#\"""---..._    ';, |      .;{___     /;\  ]#' |__....--"+"\n";
-// TitrePrincipal+=@"              .--.      ;'/#\         \    ]! |       "| , """--./_J    /         "+"\n";
-// TitrePrincipal+=@"             /  '%;    /  '/#\         \   !' :        |!# #! #! #|    :`.__      "+"\n";
-// TitrePrincipal+=@"            i__..'%] _:_   ;##J         \      :"#...._!   '  "  "|__  |    `--.._"+"\n";
-// TitrePrincipal+=@"             | .--""" !|""""  |"""----...J     | '##"" `-._       |  """---.._    "+"\n";
-// TitrePrincipal+=@"         ____: |      #|      |         #|     |          "]      ;   ___...-"T,  "+"\n";
-// TitrePrincipal+=@"        /   :  :      !|      |   _______!_    |           |__..--;"""     ,;MM;  "+"\n";
-// TitrePrincipal+=@"       :____| :    .-.#|      |  /\      /#\   |          /'               ''MM;  "+"\n";
-// TitrePrincipal+=@`        |""": |   /   \|   .----+  ;      /#\  :___..--"";                  ,'MM; "+"\n";
-// TitrePrincipal+=@"       _Y--:  |  ;     ;.-'      ;  \______/#: /         ;                  ''MM; "+"\n";
-// TitrePrincipal+=@"      /    |  | ;_______;     ____!  |"##"""MM!         ;                    ,'MM;"+"\n";
-// TitrePrincipal+=@"     !_____|  |  |"#"#"|____.'""##"  |       :         ;                     ''MM "+"\n"; 
-
-// TitrePrincipal+=@"      | """"--!._|     |##""         !       !         :____.....-------"""""" |' "+"\n";
-// TitrePrincipal+=@"      |          :     |______                        ___!_ "#""#""#"""#"""#"""|  "+"\n";
-// TitrePrincipal+=@"    __|          ;     |MM"MM"""""---..._______...--""MM"MM]                   |  "+"\n"; 
-// TitrePrincipal+=@"      "\-.      :      |#                                  :                   |  "+"\n";
-// TitrePrincipal+=@"        /#'.    |      /##,                                !                   |  "+"\n";
-// TitrePrincipal+=@"       .',/'\   |       #:#,                                ;       .==.       |  "+"\n";
-// TitrePrincipal+=@"      /"\'#"\',.|       ##;#,                               !     ,'||||',     |  "+"\n";
-// TitrePrincipal+=@"            /;/`:       ######,          ____             _ :     M||||||M     |  "+"\n";
-// TitrePrincipal+=@"           ###          /;"\.__"-._   """                   |===..M!!!!!!M_____|  "+"\n";
-// TitrePrincipal+=@"                               `--..`--.._____             _!_                    "+"\n";
-// TitrePrincipal+=@"                                              `--...____,="_.'`-.____        
-    // "+"\n";"""""""""";
+        
     return TitrePrincipal;
     }
 
