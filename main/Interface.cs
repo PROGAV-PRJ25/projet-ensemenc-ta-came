596c07ddc7e6f2541f3c9e64245ef0efe1f8aaf5
Thiméo Tonon <ttonon@ensc.fr>
2025-04-18 08:37:07 +0200

    complétion du README et de la fonction AjouterInterface

--- main/Interface.cs
+++ main/Interface.cs
@@ -1,20 +1,78 @@
-public class Interface{
-    private char[][] TexteComplet = new char[][]; //tableau rassemblant tout ce qu'il y a à afficher
+using System.Security.Principal;
 
-    private AjouterInterface(char[][] texte, int positionLigne,int positionColonne, string direction){
-        if(direction=="haut-droite");
-        else if(direction == "haut-gauche");
-        else if(direction == "bas-droite");
-        else if(direction == "bas-gauche");
-        else if(direction == "droite-haut");
-        else if(direction == "droite-bas");
-        else if(direction == "gauche-haut");
-        else if(direction == "gauche-bas");
-        
+public class Interface
+{
+    private char[,] TexteComplet = new char[Console.WindowHeight, Console.WindowWidth]; //tableau rassemblant tout ce qu'il y a à afficher
+
+    private void AjouterInterface(char[,] texte, int positionLigne, int positionColonne, string direction)
+    {
+        if (direction == "haut-droite") 
+        {
+            for (int indiceLigne = 0; indiceLigne < texte.GetLength(0); indiceLigne++)
+            {
+                for (int indiceColonne = 0; indiceColonne < texte.GetLength(0); indiceColonne++) {}
+            }
+        }
+
+        else if (direction == "haut-gauche")
+        {
+            for (int indiceLigne = 0; indiceLigne < texte.GetLength(0); indiceLigne++)
+            {
+                for (int indiceColonne = 0; indiceColonne < texte.GetLength(0); indiceColonne++) {}
+            }
+        }
+
+        else if (direction == "bas-droite")
+        {
+            for (int indiceLigne = 0; indiceLigne < texte.GetLength(0); indiceLigne++)
+            {
+                for (int indiceColonne = 0; indiceColonne < texte.GetLength(0); indiceColonne++) {}
+            }
+        }
+        else if (direction == "bas-gauche")
+        {
+            for (int indiceLigne = 0; indiceLigne < texte.GetLength(0); indiceLigne++)
+            {
+                for (int indiceColonne = 0; indiceColonne < texte.GetLength(0); indiceColonne++){}
+            }
+        }
+        else if (direction == "droite-haut")
+        {
+            for (int indiceLigne = 0; indiceLigne < texte.GetLength(0); indiceLigne++)
+            {
+                for (int indiceColonne = 0; indiceColonne < texte.GetLength(0); indiceColonne++){}
+            }
+        }
+        else if (direction == "droite-bas")
+        {
+            for (int indiceLigne = 0; indiceLigne < texte.GetLength(0); indiceLigne++)
+            {
+                for (int indiceColonne = 0; indiceColonne < texte.GetLength(0); indiceColonne++) {}
+            }
+        }
+
+        else if (direction == "gauche-haut")
+        {
+            for (int indiceLigne = 0; indiceLigne < texte.GetLength(0); indiceLigne++)
+            {
+                for (int indiceColonne = 0; indiceColonne < texte.GetLength(0); indiceColonne++) {}
+            }
+        }
+        else if (direction == "gauche-bas")
+        {
+            for (int indiceColonne = 0; indiceColonne < texte.GetLength(0); indiceColonne++)
+            {
+                for (int indiceLigne = 0; indiceLigne < texte.GetLength(0); indiceLigne++) {}
+            }
+        }
+
     }
 
-    public string RécupererASCII(string nomFichierTxt){
-
+    public string RécupererASCII(string nomFichierTxt)
+    {
+        //TODO 
+        //récupérer le nom de fichier ascii et le transformer en tableau à double entrée.
+        return "";
     }
 }
 
