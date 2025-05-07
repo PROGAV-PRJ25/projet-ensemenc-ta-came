353cc5b598ec75638f859ad16934a5d44d47cfae
ta-came <cavignes@ensc.fr>
2025-05-07 12:25:41 +0200

    Sous-classes plantes ajoutées et complétées pour pommier et blé

--- main/Plante.cs
+++ main/Plante.cs
@@ -1,10 +1,10 @@
 public abstract class Plante{
 
-    string[] Etats = ["semis","mature","déshydraté","gelé", "malade", "mort"];
+    string[] Etats {set;get;}
     public string Nom {get;private set;}
     public string Type {set;get;}
     public int SaisonSemi {set;get;}
-    public string TerrainPrefere {set;get;}
+    public string TerrainPref {set;get;}
     public int VitesseCroissance {set;get;}
     public int BesoinEau {set;get;}
     public int BesoinSoleil {set;get;}
@@ -14,37 +14,200 @@ public abstract class Plante{
     public string Defense {set;get;}
     public int Espace {set;get;}
     public int Rendement  {set;get;}
-    public string Recolte {set;get;}
+    public int Recolte {set;get;}
     public int TemperaturePref {set;get;}
-    
 
+    public Plante(string nom , string[] etats , string type , int saisonSemi , string terrainPref , 
+    int vitesseCroissance , int besoinEau , int besoinSoleil , bool crainFroid , bool crainSecheresse , 
+    string nuisible , string defence , int espace , int rendement , int recolte , int temperaturePref)
+    {
+        Nom = nom;
+        Etats = etats;
+        Type=type;
+        SaisonSemi=saisonSemi;
+        TerrainPref=terrainPref;
+        VitesseCroissance=vitesseCroissance;
+        BesoinEau=besoinEau;
+        BesoinSoleil=besoinSoleil;
+        CrainFroid=crainFroid;
+        CrainSecheresse=crainSecheresse;
+        Nuisible=nuisible;
+        Defense= defence;
+        Espace=espace;
+        Rendement=rendement;
+        Recolte=recolte;
+        TemperaturePref=temperaturePref;
+    }
+    
+}
 
     public class Pommier : Plante
     {
+        public Pommier() : 
+        base(nom: "Pommier",
+        etats: ["semis", "mature", "déshydraté", "gelé", "malade", "mort"] ,
+        type: "arbre fruitier",
+        saisonSemi: 2,
+        terrainPref: "argileux, drainé",
+        vitesseCroissance: 10,
+        besoinEau: 5,
+        besoinSoleil: 3,
+        crainFroid: false,
+        crainSecheresse: true,
+        nuisible: "chenilles et oiseaux", 
+        defence: "fermier en colère",
+        espace: 8,
+        rendement: 100,
+        recolte: 3,
+        temperaturePref: 18)
+        {
+            //corps du constructeur
+        }
+
 
     }
-    public class Blé : Plante
+    
+    
+    public class Ble : Plante
     {
+        public Ble() : 
+        base(nom: "Blé",
+        etats: ["semis", "mature", "déshydraté", "gelé", "malade", "mort"] ,
+        type: "céréale",
+        saisonSemi: 4,
+        terrainPref: "riche et humifère",
+        vitesseCroissance: 16,
+        besoinEau: 5,
+        besoinSoleil: 8,
+        crainFroid: true,
+        crainSecheresse: false,
+        nuisible: "pucerons , Oïdium", 
+        defence: "Traitement , coccinnelle",
+        espace: 4,
+        rendement: 10,
+        recolte: 3,
+        temperaturePref: 18)
+        {
+            //corps du constructeur
+        }
 
     }
     public class Carotte : Plante
     {
-
+        public Carotte() : 
+        base(nom: "Blé",
+        etats: ["semis", "mature", "déshydraté", "gelé", "malade", "mort"] ,
+        type: "céréale",
+        saisonSemi: 4,
+        terrainPref: "riche et humifère",
+        vitesseCroissance: 16,
+        besoinEau: 5,
+        besoinSoleil: 8,
+        crainFroid: true,
+        crainSecheresse: false,
+        nuisible: "pucerons , Oïdium", 
+        defence: "Traitement , coccinnelle",
+        espace: 4,
+        rendement: 10,
+        recolte: 3,
+        temperaturePref: 18)
+        {
+            //corps du constructeur
+        }
     }
-    public class Pêcher : Plante
+    public class Pecher : Plante
     {
+        public Pecher() : 
+        base(nom: "Blé",
+        etats: ["semis", "mature", "déshydraté", "gelé", "malade", "mort"] ,
+        type: "céréale",
+        saisonSemi: 4,
+        terrainPref: "riche et humifère",
+        vitesseCroissance: 16,
+        besoinEau: 5,
+        besoinSoleil: 8,
+        crainFroid: true,
+        crainSecheresse: false,
+        nuisible: "pucerons , Oïdium", 
+        defence: "Traitement , coccinnelle",
+        espace: 4,
+        rendement: 10,
+        recolte: 3,
+        temperaturePref: 18)
+        {
+            //corps du constructeur
+        }
 
     }
     public class VignesArtaban : Plante
     {
+        public VignesArtaban() : 
+        base(nom: "Blé",
+        etats: ["semis", "mature", "déshydraté", "gelé", "malade", "mort"] ,
+        type: "céréale",
+        saisonSemi: 4,
+        terrainPref: "riche et humifère",
+        vitesseCroissance: 16,
+        besoinEau: 5,
+        besoinSoleil: 8,
+        crainFroid: true,
+        crainSecheresse: false,
+        nuisible: "pucerons , Oïdium", 
+        defence: "Traitement , coccinnelle",
+        espace: 4,
+        rendement: 10,
+        recolte: 3,
+        temperaturePref: 18)
+        {
+            //corps du constructeur
+        }
 
     }
     public class Olivier : Plante
     {
+        public Olivier() : 
+        base(nom: "Blé",
+        etats: ["semis", "mature", "déshydraté", "gelé", "malade", "mort"] ,
+        type: "céréale",
+        saisonSemi: 4,
+        terrainPref: "riche et humifère",
+        vitesseCroissance: 16,
+        besoinEau: 5,
+        besoinSoleil: 8,
+        crainFroid: true,
+        crainSecheresse: false,
+        nuisible: "pucerons , Oïdium", 
+        defence: "Traitement , coccinnelle",
+        espace: 4,
+        rendement: 10,
+        recolte: 3,
+        temperaturePref: 18)
+        {
+            //corps du constructeur
+        }
 
     }
     public class Tournesol : Plante
     {
+        public Tournesol() : 
+        base(nom: "Blé",
+        etats: ["semis", "mature", "déshydraté", "gelé", "malade", "mort"] ,
+        type: "céréale",
+        saisonSemi: 4,
+        terrainPref: "riche et humifère",
+        vitesseCroissance: 16,
+        besoinEau: 5,
+        besoinSoleil: 8,
+        crainFroid: true,
+        crainSecheresse: false,
+        nuisible: "pucerons , Oïdium", 
+        defence: "Traitement , coccinnelle",
+        espace: 4,
+        rendement: 10,
+        recolte: 3,
+        temperaturePref: 18)
+        {
+            //corps du constructeur
+        }
 
     }
-}
