f7963a1c22dae09893458514b355cd34efb6d33e
ta-came <cavignes@ensc.fr>
2025-05-07 10:45:06 +0200

    Plante >> caractéristiques

--- main/Plante.cs
+++ main/Plante.cs
@@ -1,14 +1,31 @@
 public abstract class Plante{
 
-    string[] etats = ["semis","mature","déshydraté","gelé", "malade", "mort"];
+    string[] Etats = ["semis","mature","déshydraté","gelé", "malade", "mort"];
     public string Nom {get;private set;}
-    public string nature {set;get;}
-    public int saisonSemi {set;get;}
-    public string terrainPrefere {set;get;}
-    public int vitesseCroissance {set;get;}
-    public int besoinEau {set;get;}
-    public Plante(string nom){
+    public string Type {set;get;}
+    public int SaisonSemi {set;get;}
+    public string TerrainPrefere {set;get;}
+    public int VitesseCroissance {set;get;}
+    public int BesoinEau {set;get;}
+    public int BesoinSoleil {set;get;}
+    public bool CrainFroid {set;get;}
+    public bool CrainSecheresse {set;get;}
+    public string Nuisible {set;get;}
+    public string Défense {set;get;}
+    public int Espace {set;get;}
+    public int Rendement  {set;get;}
+    public string Recolte {set;get;}
+    public int TemperaturePref {set;get;}
+
+
+        public Plante(string nom){
         Nom=nom;
     }
     
+
+
+    public class Olivier : Plante
+    {
+
+    }
 }
